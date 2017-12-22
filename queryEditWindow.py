a53f33112cfb601c860d66a5efdc92f61b1a9c0d
Jiri Prajzner <jprajzne@redhat.com>
2017-12-22 17:31:47 +0100

    UPD: renaming vars in order to prepare for feature edit window

--- queryEditWindow.py
+++ queryEditWindow.py
@@ -19,45 +19,44 @@ from gi.repository import GLib
 class QueryEditWindow:
     def __init__(self):
         self.app_name = 'example-app'
-        text_view = Gtk.TextView()
-        self.text_buffer = text_view.get_buffer()
-        text_view.show()
+        steps_text_view = Gtk.TextView()
+        self.steps_text_buffer = steps_text_view.get_buffer()
+        steps_text_view.show()
 
-        scroll_window = Gtk.ScrolledWindow()
-        scroll_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-        scroll_window.add(text_view)
-        scroll_window.show()
+        steps_scroll_window = Gtk.ScrolledWindow()
+        steps_scroll_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        steps_scroll_window.add(steps_text_view)
+        steps_scroll_window.show()
 
-        cancel_button = Gtk.Button('Clear query')
-        cancel_button.connect('clicked', self.clear_text_buffer)
-        cancel_button.show()
+        steps_cancel_button = Gtk.Button('Clear query')
+        steps_cancel_button.connect('clicked', self.clear_text_buffer)
+        steps_cancel_button.show()
 
-        write_button = Gtk.Button('Write Behave Steps')
-        write_button.connect('clicked', self.write_behave_steps)
-        #write_button.set_flags(Gtk.CAN_DEFAULT)
-        write_button.show()
+        steps_write_button = Gtk.Button('Write Behave Steps')
+        steps_write_button.connect('clicked', self.write_behave_steps)
+        steps_write_button.show()
 
-        h_button_box = Gtk.HButtonBox()
-        h_button_box.pack_start(write_button, True, True, 0)
-        h_button_box.pack_start(cancel_button, True, True, 0)
-        h_button_box.show()
+        steps_h_button_box = Gtk.HButtonBox()
+        steps_h_button_box.pack_start(steps_write_button, True, True, 0)
+        steps_h_button_box.pack_start(steps_cancel_button, True, True, 0)
+        steps_h_button_box.show()
 
-        scroll_vbox = Gtk.VBox(False, 0)
-        scroll_vbox.add(scroll_window)
-        scroll_vbox.show()
-        scroll_vbox.pack_start(h_button_box, False, False, 0)
+        vbox = Gtk.VBox(False, 0)
+        vbox.add(steps_scroll_window)
+        vbox.show()
+        vbox.pack_start(steps_h_button_box, False, False, 0)
 
         window = Gtk.Window()
         window.set_resizable(True)
         window.connect('destroy', self.close_query_edit_window)
         window.set_title('Dogtail Query Edit Window')
         window.set_border_width(0)
-        window.add(scroll_vbox)
+        window.add(vbox)
         window.show()
 
 
     def clear_text_buffer(self, widget):
-        self.text_buffer.set_text('', 0)
+        self.steps_text_buffer.set_text('', 0)
 
 
     def close_query_edit_window(self, widget):
@@ -66,7 +65,7 @@ class QueryEditWindow:
 
     def fill_text_buffer(self, widget, app_name, content):
         self.app_name = app_name
-        self.text_buffer.set_text(content, len(content))
+        self.steps_text_buffer.set_text(content, len(content))
 
 
     def write_behave_steps(self, widget):
@@ -78,7 +77,7 @@ class QueryEditWindow:
             os.makedirs(join(projectDir, 'features', 'steps'))
 
         steps = open(join(projectDir, 'features', 'steps', 'steps.py'), 'wb') #dont forget to change it to 'ab' mode
-        steps.write(self.text_buffer.get_text(self.text_buffer.get_start_iter(), self.text_buffer.get_end_iter(), include_hidden_chars = True))
+        steps.write(self.steps_text_buffer.get_text(self.steps_text_buffer.get_start_iter(), self.steps_text_buffer.get_end_iter(), include_hidden_chars = True))
         steps.close()
 
 
