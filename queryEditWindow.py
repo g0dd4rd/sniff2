5ff06b4a51d26cdef69d772003eab3d580fd2748
Jiri Prajzner <jprajzne@redhat.com>
2018-01-02 16:39:14 +0100

    UPD: renaming everything related to the dogtail query part

--- queryEditWindow.py
+++ queryEditWindow.py
@@ -19,66 +19,67 @@ from gi.repository import GLib
 class QueryEditWindow:
     def __init__(self):
         self.app_name = 'example-app'
-        steps_text_view = Gtk.TextView()
-        self.steps_text_buffer = steps_text_view.get_buffer()
-        steps_text_view.show()
+        query_text_view = Gtk.TextView()
+        self.query_text_buffer = query_text_view.get_buffer()
+        query_text_view.show()
 
-        steps_scroll_window = Gtk.ScrolledWindow()
-        steps_scroll_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-        steps_scroll_window.add(steps_text_view)
-        steps_scroll_window.show()
+        query_scroll_window = Gtk.ScrolledWindow()
+        query_scroll_window.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        query_scroll_window.add(query_text_view)
+        query_scroll_window.show()
 
-        steps_cancel_button = Gtk.Button('Clear query')
-        steps_cancel_button.connect('clicked', self.clear_text_buffer)
-        steps_cancel_button.show()
+        query_cancel_button = Gtk.Button('Clear query')
+        query_cancel_button.connect('clicked', self.clear_query_text_buffer)
+        query_cancel_button.show()
 
-        steps_write_button = Gtk.Button('Write Behave Steps')
-        steps_write_button.connect('clicked', self.write_behave_steps)
-        steps_write_button.show()
+        query_write_button = Gtk.Button('Write Dogtail Query')
+        query_write_button.connect('clicked', self.write_dogtail_query)
+        query_write_button.show()
 
-        steps_h_button_box = Gtk.HButtonBox()
-        steps_h_button_box.pack_start(steps_write_button, True, True, 0)
-        steps_h_button_box.pack_start(steps_cancel_button, True, True, 0)
-        steps_h_button_box.show()
+        query_h_button_box = Gtk.HButtonBox()
+        query_h_button_box.pack_start(query_write_button, True, True, 0)
+        query_h_button_box.pack_start(query_cancel_button, True, True, 0)
+        query_h_button_box.show()
 
         vbox = Gtk.VBox(False, 0)
-        vbox.add(steps_scroll_window)
+        vbox.add(query_scroll_window)
         vbox.show()
-        vbox.pack_start(steps_h_button_box, False, False, 0)
+        vbox.pack_start(query_h_button_box, False, False, 0)
 
         window = Gtk.Window()
         window.set_resizable(True)
-        window.connect('destroy', self.close_query_edit_window)
-        window.set_title('Dogtail Query Edit Window')
+        window.connect('destroy', self.close_edit_window)
+        window.set_title('Dogtail Query - Behave Steps Edit Window')
         window.set_border_width(0)
         window.add(vbox)
         window.show()
 
 
-    def clear_text_buffer(self, widget):
-        self.steps_text_buffer.set_text('', 0)
+    def clear_query_text_buffer(self, widget):
+        self.query_text_buffer.set_text('', 0)
 
 
-    def close_query_edit_window(self, widget):
+    def close_edit_window(self, widget):
         Gtk.main_quit()
 
 
-    def fill_text_buffer(self, widget, app_name, content):
+    def fill_query_text_buffer(self, widget, app_name, content):
         self.app_name = app_name
-        self.steps_text_buffer.set_text(content, len(content))
+        self.query_text_buffer.set_text(content, len(content))
 
 
-    def write_behave_steps(self, widget):
-        print('=== writing behave steps ===')
+    def write_dogtail_query(self, widget):
+        print('=== writing dogtail query ===')
         import os
         from os.path import expanduser, join, exists
         projectDir = join(expanduser('~'), 'dogtail-behave-projects', self.app_name)
         if not exists(join(projectDir, 'features', 'steps')):
             os.makedirs(join(projectDir, 'features', 'steps'))
 
-        steps = open(join(projectDir, 'features', 'steps', 'steps.py'), 'wb') #dont forget to change it to 'ab' mode
-        steps.write(self.steps_text_buffer.get_text(self.steps_text_buffer.get_start_iter(), self.steps_text_buffer.get_end_iter(), include_hidden_chars = True))
-        steps.close()
+        query = open(join(projectDir, 'features', 'steps', 'steps.py'), 'wb') #dont forget to change it to 'ab' mode
+        query.write(self.query_text_buffer.get_text(self.query_text_buffer.get_start_iter(), self.query_text_buffer.get_end_iter(), include_hidden_chars = True))
+        query.close()
+        print('=== done writing dogtail query ===\n')
 
 
 def main():
